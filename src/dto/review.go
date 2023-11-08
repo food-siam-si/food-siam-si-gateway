ad69cd962c4555f26d24a849b86a360fd1e5e4c5
miello <partearth1212@gmail.com>
2023-11-08 20:46:49 +0700

    fix: dockerfile and wrong request

--- src/dto/review.go
+++ src/dto/review.go
@@ -2,14 +2,14 @@ package dto
 
 type CreateReviewRequest struct {
 	Description string  `json:"description" validate:"required"`
-	Rating      float32 `json:"rating" validate:"required,gte=1,lte=5"`
+	Rating      float32 `json:"rate" validate:"required,gte=1,lte=5"`
 }
 
 type CreateReviewRequestService struct {
 	UserId       uint32  `json:"userId"`
 	RestaurantId uint32  `json:"restaurantId"`
 	Description  string  `json:"description"`
-	Rating       float32 `json:"rating"`
+	Rating       float32 `json:"rate"`
 }
 
 type RestaurantReview struct {
